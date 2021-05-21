7b276aa3edabc498275caa53af50f2423a33a070
Mourat Boutry <mourat@boutry.me>
2021-05-21 12:09:56 +0200

    AdminLTE3 theme integrated with Mix configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+/* Styles start */
+mix.styles([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.css',
+    'resources/assets/admin/css/adminlte.css',
+], 'public/assets/admin/css/admin.css');
+mix.copy('resources/assets/admin/css/adminlte.min.css.map', 'public/assets/admin/css/adminlte.min.css.map');
+mix.copy('resources/assets/admin/css/adminlte.css.map', 'public/assets/admin/css/adminlte.css.map');
+/* Styles and */
+
+/* Scripts start */
+mix.scripts([
+    'resources/assets/admin/plugins/jquery/jquery.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.js',
+    'resources/assets/admin/js/adminlte.js',
+    'resources/assets/admin/js/demo.js',
+], 'public/assets/admin/js/admin.js');
+mix.copy('resources/assets/admin/plugins/jquery/jquery.min.map', 'public/assets/admin/js/jquery.min.map');
+mix.copy('resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js.map', 'public/assets/admin/js/bootstrap.bundle.min.js.map');
+mix.copy('resources/assets/admin/js/adminlte.min.js.map', 'public/assets/admin/js/adminlte.min.js.map');
+mix.copy('resources/assets/admin/js/adminlte.js.map', 'public/assets/admin/js/adminlte.js.map');
+/* Scripts end */
+
+/* Copy some files start */
+mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
+mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
+/* Copy some files end */
