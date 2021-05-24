ffcce66c4fbfdd7bf7abca7cc656d5766c153b87
Mourat Boutry <mourat@boutry.me>
2021-05-24 12:42:00 +0200

    Created database relationships & installed "Sluggable" extension.

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,32 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
