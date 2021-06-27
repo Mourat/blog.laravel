fc73f1734e819ec4cb9bce0fc8d146e149414107
Mourat <mourat@boutry.me>
2021-06-27 23:37:14 +0200

    Login, logout and user state control

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreUser;
+use App\Http\Requests\StoreLogin;
+use App\Http\Requests\StoreRegistration;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +14,7 @@ class UserController extends Controller
         return view('user.create');
     }
 
-    public function store(StoreUser $request){
+    public function store(StoreRegistration $request){
 
         $user = User::create([
             'name' => $request->name,
@@ -24,5 +25,32 @@ class UserController extends Controller
         session()->flash('success', 'User registered successfully');
         Auth::login($user);
         return redirect()->home();
+    }
+
+    public function loginForm()
+    {
+        return view('user.login');
+    }
+
+    public function login(StoreLogin $request)
+    {
+        if(Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password,
+        ])){
+            session()->flash('success', 'You are logged in.');
+            if(Auth::user()->is_admin){
+                return redirect()->route('admin.index');
+            } else{
+                return redirect()->home();
+            }
+        }
+        return redirect()->back()->with('error', 'Incorrect login information');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login.create');
     }
 }
